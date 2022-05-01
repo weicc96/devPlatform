88efdf7dfb03024539ebf02383603b38ce387668
weicc96 <2317148900@qq.com>
2022-05-01 17:37:13 +0800

    init

---
+++ src/main/java/org/dev/plantform/PlantformApplication.java
@@ -0,0 +1,19 @@
+package org.dev.plantform;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@SpringBootApplication
+public class PlantformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantformApplication.class, args);
+    }
+
+}
