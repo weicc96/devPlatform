f5a3d159d3a27711b1bfe87a5e35b7a1c28fd586
weicc96 <2317148900@qq.com>
2022-05-03 08:23:46 +0800

    配置代码生成器

--- src/main/java/org/dev/plantform/PlantformApplication.java
+++ src/main/java/org/dev/plantform/PlantformApplication.java
@@ -3,13 +3,17 @@ package org.dev.plantform;
 import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Import(FdfsClientConfig.class)
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
 public class PlantformApplication {
 
     public static void main(String[] args) {
